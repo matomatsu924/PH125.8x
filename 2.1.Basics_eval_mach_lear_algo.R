651932d73b69aed7a1743169ed6ad75982862191
Mauricio Matoma <97911503+matomatsu924@users.noreply.github.com>
2025-05-07 01:39:17 -0500

    V2

--- 2.1.Basics_eval_mach_lear_algo.R
+++ 2.1.Basics_eval_mach_lear_algo.R
@@ -8,6 +8,7 @@ library(dslabs)
 
 data(heights)
 
+
 # Definir predictores y resultados
 
 y <- heights$sex
@@ -71,3 +72,362 @@ y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>%
 y_hat <- factor(y_hat)
 
 mean(y_hat == test_set$sex)
+
+################################################################################
+# tabulate each combination of prediction and actual value
+table(predicted = y_hat, actual = test_set$sex)
+test_set %>% 
+  mutate(y_hat = y_hat) %>%
+  group_by(sex) %>% 
+  summarize(accuracy = mean(y_hat == sex))
+prev <- mean(y == "Male")
+
+confusionMatrix(data = y_hat, reference = test_set$sex)
+
+################################################################################
+
+cm <- confusionMatrix(data= y_hat, reference = test_set$sex)
+
+cm$overall["Accuracy"]
+
+cm$byClass[c("Sensitivity", "Specificity", "Prevalence")]
+
+################################################################################
+#Maximizar F-score
+
+cutoff <- seq(61,70)
+
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  F_meas(data= y_hat, reference = factor(train_set$sex))
+})
+
+data.frame(cutoff, F_1) %>% 
+  ggplot(aes(cutoff, F_1)) +
+  geom_point() +
+  geom_line() +
+  theme_bw()
+
+max(F_1)
+
+
+best_cutoff_2 <- cutoff[which.max(F_1)]
+best_cutoff_2
+
+y_hat <- ifelse(test_set$height > best_cutoff_2, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+
+sensitivity(data= y_hat, reference = test_set$sex)
+specificity(data = y_hat, reference = test_set$sex)
+
+
+################################################################################
+p <- 0.9
+n <- length(test_index)
+y_hat <- sample(c("Male", "Female"), n, replace = TRUE, prob=c(p, 1-p)) %>% 
+  factor(levels = levels(test_set$sex))
+mean(y_hat == test_set$sex)
+
+# ROC curve
+probs <- seq(0, 1, length.out = 10)
+guessing <- map_df(probs, function(p){
+  y_hat <- 
+    sample(c("Male", "Female"), n, replace = TRUE, prob=c(p, 1-p)) %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Guessing",
+       FPR = 1 - specificity(y_hat, test_set$sex),
+       TPR = sensitivity(y_hat, test_set$sex))
+})
+guessing %>% qplot(FPR, TPR, data =., xlab = "1 - Specificity", ylab = "Sensitivity")
+
+cutoffs <- c(50, seq(60, 75), 80)
+height_cutoff <- map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Height cutoff",
+       FPR = 1-specificity(y_hat, test_set$sex),
+       TPR = sensitivity(y_hat, test_set$sex))
+})
+
+# plot both curves together
+bind_rows(guessing, height_cutoff) %>%
+  ggplot(aes(FPR, TPR, color = method)) +
+  geom_line() +
+  geom_point() +
+  xlab("1 - Specificity") +
+  ylab("Sensitivity")
+
+library(ggrepel)
+map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Height cutoff",
+       cutoff = x, 
+       FPR = 1-specificity(y_hat, test_set$sex),
+       TPR = sensitivity(y_hat, test_set$sex))
+}) %>%
+  ggplot(aes(FPR, TPR, label = cutoff)) +
+  geom_line() +
+  geom_point() +
+  geom_text_repel(nudge_x = 0.01, nudge_y = -0.01)
+
+# plot precision against recall
+guessing <- map_df(probs, function(p){
+  y_hat <- sample(c("Male", "Female"), length(test_index), 
+                  replace = TRUE, prob=c(p, 1-p)) %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Guess",
+       recall = sensitivity(y_hat, test_set$sex),
+       precision = precision(y_hat, test_set$sex))
+})
+
+height_cutoff <- map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Height cutoff",
+       recall = sensitivity(y_hat, test_set$sex),
+       precision = precision(y_hat, test_set$sex))
+})
+
+bind_rows(guessing, height_cutoff) %>%
+  ggplot(aes(recall, precision, color = method)) +
+  geom_line() +
+  geom_point()
+guessing <- map_df(probs, function(p){
+  y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE, 
+                  prob=c(p, 1-p)) %>% 
+    factor(levels = c("Male", "Female"))
+  list(method = "Guess",
+       recall = sensitivity(y_hat, relevel(test_set$sex, "Male", "Female")),
+       precision = precision(y_hat, relevel(test_set$sex, "Male", "Female")))
+})
+
+height_cutoff <- map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Male", "Female"))
+  list(method = "Height cutoff",
+       recall = sensitivity(y_hat, relevel(test_set$sex, "Male", "Female")),
+       precision = precision(y_hat, relevel(test_set$sex, "Male", "Female")))
+})
+bind_rows(guessing, height_cutoff) %>%
+  ggplot(aes(recall, precision, color = method)) +
+  geom_line() +
+  geom_point()
+
+
+
+################################################################################
+library(dslabs)
+library(dplyr)
+library(lubridate)
+data(reported_heights)
+#View(reported_heights)
+
+dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
+  filter(date_time >= make_date(2016, 01, 25) & date_time < make_date(2016, 02, 1)) %>%
+  mutate(type = ifelse(day(date_time) == 25 & hour(date_time) == 8 & between(minute(date_time), 15, 30), "inclass","online")) %>%
+  select(sex, type)
+
+#dat
+#View(dat)
+
+y <- factor(dat$sex, c("Female", "Male"))
+#y
+x <- dat$type
+#x
+
+y[which(x=="inclass")] 
+
+inclass_data <- data.frame(y[which(x=="inclass")] ) 
+mean(inclass_data[1]=="Female")
+
+
+mean(y[which(x=="online")]=="Female")
+
+table(y[which(x=="online")])
+
+#Otra forma de hacerlo
+
+dat %>% group_by(type) %>% 
+  summarize(prop_female= mean(sex=="Female"))
+
+
+################################################################################
+y_hat <- ifelse(dat$type =="inclass", "Female", "Male") %>% 
+  factor(levels = levels(y))
+
+mean(y_hat == dat$sex)
+
+table(y_hat, y)
+
+sensitivity(y_hat,y)
+specificity(y_hat,y)
+
+
+mean(y=="Female")
+confusionMatrix(y_hat,y)
+
+################################################################################
+library(caret)
+data(iris)
+iris <- iris[-which(iris$Species=='setosa'),]
+y <- iris$Species
+#y
+
+set.seed(76)
+test_index <- createDataPartition(y, times=1, p=0.5, list=FALSE)
+test <- iris[test_index,]
+train <- iris[-test_index,]
+#View(train)
+
+
+#Accuracy sepal length
+range(test$Sepal.Length)
+
+cutoff <- seq(4.9,7.9, 0.1)
+cutoff
+
+
+accuracy_sepal_l <- map_dbl(cutoff,function(x){
+  y_hat = ifelse(train$Sepal.Length > x, "virginica", "versicolor")
+  factor(levels = levels(train$Species))
+  mean(y_hat==train$Species)
+})
+
+max(accuracy_sepal_l)
+
+#Accuracy sepal width
+range(train$Sepal.Width)
+
+cutoff <- seq(2,3.8,.1)
+
+accuracy_sepal_w <- map_dbl(cutoff,function(x){
+  y_hat = ifelse(train$Sepal.Width > x, "virginica", "versicolor")
+  factor(levels = levels(train$Species))
+  mean(y_hat==train$Species)
+})
+
+max(accuracy_sepal_w)
+
+#Accuracy petal length
+
+range(train$Petal.Length)
+
+cutoff <- seq(3,6.7,.1)
+
+accuracy_petal_l <- map_dbl(cutoff,function(x){
+  y_hat = ifelse(train$Petal.Length > x, "virginica", "versicolor")
+  factor(levels = levels(train$Species))
+  mean(y_hat==train$Species)
+})
+
+max(accuracy_petal_l)
+cutoff[which.max(accuracy_petal_l)]
+
+#Accuracy petal width
+
+range(train$Petal.Width)
+
+cutoff <- seq(1,2.5,.1)
+
+accuracy_petal_w <- map_dbl(cutoff,function(x){
+  y_hat = ifelse(train$Petal.Width > x, "virginica", "versicolor")
+  factor(levels = levels(train$Species))
+  mean(y_hat==train$Species)
+})
+
+max(accuracy_petal_w)
+
+#Otra forma de hacerlo
+
+foo <- function(x){
+  rangedValues <- seq(range(x)[1], range(x)[2], by=0.1)
+  sapply(rangedValues, function(i){
+    y_hat <- ifelse(x>i, 'virginica', 'versicolor')
+    mean(y_hat==train$Species)
+  })
+}
+
+predictions <- apply(train[,-5], 2, foo)
+sapply(predictions, max)	
+
+################################################################################
+
+range(train$Petal.Width)
+
+cutoff <- seq(1,2.5,.1)
+
+accuracy_petal_w <- map_dbl(cutoff,function(x){
+  y_hat = ifelse(train$Petal.Width > x, "virginica", "versicolor")
+  factor(levels = levels(train$Species))
+  mean(y_hat==train$Species)
+})
+
+
+cutoff_max <- cutoff[which.max(accuracy_petal_w)]  
+cutoff_max
+
+#Con el resultado del valor máximo de los datos de entrenamiento, procedo a
+#evaluar con los datos test
+
+y_hat = ifelse(test$Petal.Width > cutoff_max, "virginica", "versicolor")
+factor(levels = levels(test$Species))
+mean(y_hat==test$Species)
+
+
+#Otra forma de hacerlo
+
+predictions <- foo(train[,4])
+rangedValues <- seq(range(train[,4])[1], range(train[,4])[2], by=0.1)
+cutoffs <-rangedValues[which(predictions==max(predictions))]
+
+y_hat <- ifelse(test[,4]>cutoffs[1], 'virginica', 'versicolor')
+mean(y_hat==test$Species)
+
+################################################################################
+plot(iris, pch=21, bg=iris$Species)
+################################################################################
+train[which.max(predictions$Petal.Length)]  
+
+
+
+y_hat = ifelse(test$Petal.Length > 4.6 & test$Petal.Width > 1.5, "virginica", "versicolor")
+factor(levels = levels(test$Species))
+mean(y_hat==test$Species)
+
+################################################################################
+#Otra forma
+
+library(caret)
+data(iris)
+iris <- iris[-which(iris$Species=='setosa'),]
+y <- iris$Species
+
+plot(iris, pch=21, bg=iris$Species)
+
+set.seed(76) 
+test_index <- createDataPartition(y, times=1, p=0.5, list=FALSE)
+test <- iris[test_index,]
+train <- iris[-test_index,]
+
+petalLengthRange <- seq(range(train$Petal.Length)[1], range(train$Petal.Length)[2],by=0.1)
+petalWidthRange <- seq(range(train$Petal.Width)[1], range(train$Petal.Width)[2],by=0.1)
+
+length_predictions <- sapply(petalLengthRange, function(i){
+  y_hat <- ifelse(train$Petal.Length>i, 'virginica', 'versicolor')
+  mean(y_hat==train$Species)
+})
+length_cutoff <- petalLengthRange[which.max(length_predictions)] # 4.6
+
+width_predictions <- sapply(petalWidthRange, function(i){
+  y_hat <- ifelse(train$Petal.Width>i, 'virginica', 'versicolor')
+  mean(y_hat==train$Species)
+})
+width_cutoff <- petalWidthRange[which.max(width_predictions)] # 1.5
+
+y_hat <- ifelse(test$Petal.Length>length_cutoff & test$Petal.Width>width_cutoff, 'virginica', 'versicolor')
+mean(y_hat==test$Species)
+
+
+
--- Key_points.md
+++ Key_points.md
@@ -7,6 +7,8 @@
 
 ### 2.1. Basics of Evaluating Machine Learning Algorithms
 
+#### Evaluation metrics
+
 #### Key points
 
 - To mimic the ultimate evaluation process, we randomly split our data
@@ -26,3 +28,81 @@
 - The simplest evaluation metric for categorical outcomes is overall
   accuracy: the proportion of cases that were correctly predicted in the
   test set.
+
+#### Confusion matrix
+
+- Overall accuracy can sometimes be a deceptive measure because of
+  unbalanced classes.
+
+- A general improvement to using overall accuracy is to study
+  sensitivity and specificity separately. These will be defined in the
+  next video.
+
+- A confusion matrix tabulates each combination of prediction and actual
+  value. You can create a confusion matrix in R using the `table()`
+  function or the `confusionMatrix()` function from the **caret**
+  package.
+
+- If your training data is biased in some way, you are likely to develop
+  algorithms that are biased as well. The problem of biased training
+  sets is so commom that there are groups dedicated to study it.
+
+#### Sensitivity, specificity and prevalence
+
+![](images/clipboard-3831234289.png)
+
+![](images/clipboard-3397224890.png)
+
+#### Balanced accuracy and F1 score
+
+- For optimization purposes, sometimes it is more useful to have a one
+  number summary than studying both specificity and sensitivity. One
+  preferred metric is **balanced accuracy**. Because specificity and
+  sensitivity are rates, it is more appropriate to compute the
+  ***harmonic*** average. In fact, the **F1-score**, a widely used
+  one-number summary, is the harmonic average of precision and recall. 
+
+- Depending on the context, some type of errors are more costly than
+  others. The **F1-score** can be adapted to weigh specificity and
+  sensitivity differently. 
+
+- You can compute the **F1-score** using the `F_meas()` function in the
+  **caret** package.
+
+#### Prevalence matters in practice
+
+- A machine learning algorithm with very high sensitivity and
+  specificity may not be useful in practice when prevalence is close to
+  either 0 or 1. For example, if you develop an algorithm for disease
+  diagnosis with very high sensitivity, but the prevalence of the
+  disease is pretty low, then the precision of your algorithm is
+  probably very low based on Bayes’ theorem.
+
+#### ROC and precision-recall curves
+
+- A very common approach to evaluating accuracy and F1-score is to
+  compare them graphically by plotting both. A widely used plot that
+  does this is the **receiver operating characteristic (ROC) curve**.
+  The ROC curve plots sensitivity (TPR) versus 1 - specificity, also
+  known as the false positive rate (FPR).
+
+- However, ROC curves have one weakness and it is that neither of the
+  measures plotted depend on prevalence. In cases in which prevalence
+  matters, we may instead make a **precision-recall plot**, which has a
+  similar idea with ROC curve.
+
+#### Loss Function
+
+- The most commonly used loss function is the squared loss function.
+  Because we often have a test set with many observations, say N, we use
+  the mean squared error (MSE). In practice, we often report the root
+  mean squared error (RMSE), which is the square root of MSE, because it
+  is in the same units as the outcomes.
+
+- If the outcomes are binary, both RMSE and MSE are equivalent to one
+  minus accuracy
+
+- Note that there are loss functions other than the squared loss. For
+  example, the Mean Absolute Error uses absolute values instead of
+  squaring the errors. However, we focus on minimizing square loss since
+  it is the most widely used.
